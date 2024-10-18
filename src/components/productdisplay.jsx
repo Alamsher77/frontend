1e4e993a2ff7170fbead0d85efc7234f38db2be8
Alamsher77 <alamsheransari15@gmail.com>
2024-10-18 18:04:04 +0530

    change

--- src/components/productdisplay.jsx
+++ src/components/productdisplay.jsx
@@ -9,6 +9,8 @@ import { IoCloseSharp } from "react-icons/io5";
 import {useNavigate} from "react-router-dom";
 import DisplayCurrency from '../displayCurrancy'  
 import ZoomImage from './zoomimage'
+import { FaCartPlus } from "react-icons/fa";
+import { TbPlayerTrackNextFilled } from "react-icons/tb";
 const ProductDisplay = (props)=>{
 const [viewProduct,setViewProduct] = useState({
   close:false,
@@ -54,10 +56,19 @@ const [viewProduct,setViewProduct] = useState({
      toast.error(error.message)
    }
  }
-  
-  
+ 
+ const [scroll,setscroll] = useState(0)
+ 
+ 
+ useEffect(()=>{
+   window.addEventListener('scroll',()=>{
+     setscroll(window.scrollY)
+   })
+ },[scroll])
+ 
+ console.log(scroll)
    return(
-     <div className="" >
+     <div className="relative " >
        <div className="flex w-full gap-1 max-w-full max-h-[300px] overflow-x-scroll" style={{scrollbarWidth:'none'}}>
           {props.image.map((image,indes)=>{
             return(
@@ -96,17 +107,25 @@ const [viewProduct,setViewProduct] = useState({
        <strong className="text-red-600">{DisplayCurrency(props.newPrice)}</strong> <span className="text-green-600">-{m}%</span>
      
       </div>
-  <button onClick={()=>{addToCartController(props.id)}} class="bg-green-500 hover:bg-green-400 text-white font-bold py-2 px-6  my-2">
-  Buy now
-  </button>
  </div>
  
  <div className="product-description">
     <b  className="text-slate-700">Product Description :</b>
     <p>{props.productInfo}</p>
  </div>
-      
+ 
+ { /* button for addtocard and bay*/ }
+     <div className={`fixed ${scroll < 200 ? 'bottom-0': '-bottom-20'} transition ease-in-out delay-200 bg-white flex justify-between  shadow rounded-t-2xl shadow-black z-[1000] w-full py-2 pb-5 px-4`}>
+     
+     <button onClick={()=>{addToCartController(props.id)}} className="py-1 transition ease-in-out delay-150 hover:bg-white hover:text-pink-400 border-pink-400 flex tracking-widest font-bold uppercase bg-pink-400 text-white justify-center items-center text-[18px] px-8 rounded-md  border gap-3">
+      <FaCartPlus /> <span>cart</span>
+     </button>
+     
+     <button className="py-1 transition ease-in-out delay-150 hover:bg-white hover:text-green-500 border-green-500 flex tracking-widest font-bold uppercase bg-green-500 text-white justify-center items-center text-[18px] px-8 rounded-md  border gap-3">
+      <TbPlayerTrackNextFilled className="text-2xl" /> <span>Buy</span>
+     </button>
      </div>
+  </div>
     
     )
 }
