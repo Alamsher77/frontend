138ce979967c042ec4798cb1e56efb496fa39215
Alamsher77 <alamsheransari15@gmail.com>
2025-04-04 15:46:02 +0530

    filechanges for error

--- src/components/loddingCardComponent.jsx
+++ src/components/loddingCardComponent.jsx
@@ -1,15 +1,15 @@
 
 const LoddingCardComponent = ({key})=>{
   return(
-     <div key={key} className='animate-pulse p-1 m-2 '>
-     <div className=" h-40 bg-gray-300">
+     <div key={key} className='shimmer animate-pulse p-1 m-2 '>
+     <div className=" h-40 bg-gray-200">
        <span className="w-full h-full "></span>
      </div>
       <div className="break-all mt-1  bg-gray-100">
-        <p className="bg-gray-300 p-3"> </p>
-       <p className="p-3 w-20 bg-gray-300 mt-1"> </p>
+        <p className="bg-gray-200 p-3"> </p>
+       <p className="p-3 w-20 bg-gray-200 mt-1"> </p>
        <div className="flex gap-1">
-       <span className="bg-gray-300 p-2 mt-1 w-16"></span> <span className="w-16 mt-1 bg-gray-300"></span>
+       <span className="bg-gray-200 p-2 mt-1 w-16"></span> <span className="w-16 mt-1 bg-gray-200"></span>
      </div>
       </div>
      </div>
